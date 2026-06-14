572700f9697d7d0797cf4c019ce1cfa43e61d4db
manojsonawane810 <79688676+manojsonawane810@users.noreply.github.com>
2026-06-14 15:28:45 +0530

    Assignment-2 programs
    Uploaded assignment-2 program files

---
+++ Assignment-2/AssignmentNumbers.py
@@ -0,0 +1,17 @@
+print("Enter first number: ")
+no1 = int(input())
+
+print("Enter second number: ")
+no2 = int(input())
+
+addition = no1 + no2
+print(addition)
+
+substraction = no1 - no2
+print(substraction)
+
+multiplicatio = no1 * no2
+print(multiplicatio)
+
+division = no1 / no2
+print(division)
